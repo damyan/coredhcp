66f9c25a305eb65f23368df9d256686862532e90
Damyan Yordanov <damyan.yordanov@sap.com>
2024-09-11 13:50:35 +0200

    Fix MACOS build by adding a stub

---
+++ server/sendEthernet_darwin.go
@@ -0,0 +1,15 @@
+//go:build darwin
+// +build darwin
+
+package server
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/insomniacslk/dhcp/dhcpv4"
+)
+
+func sendEthernet(iface net.Interface, resp *dhcpv4.DHCPv4) error {
+	return fmt.Errorf("not implemented")
+}
